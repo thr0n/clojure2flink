605b2f2cd7d5113d28edced921be75751e9c7e31
Hendrik Thorun <hendrik.thorun@gmail.com>
2016-12-19 12:05:27 +0100

    kafka stuff added

---
+++ src/bridge/connectors/kafka.clj
@@ -0,0 +1,27 @@
+(ns bridge.connectors.kafka
+  (:import (java.util Properties)
+           (org.apache.flink.streaming.connectors.kafka FlinkKafkaConsumer09 FlinkKafkaProducer09)))
+
+(defn create-kafka-consumer [topic deserialization-schema properties]
+  "Creates a Kafka data source."
+  (FlinkKafkaConsumer09. topic deserialization-schema properties))
+
+(defn create-kafka-producer [broker-address topic serialization-schema]
+  "Creates a Kafka sink."
+  (FlinkKafkaProducer09. broker-address topic serialization-schema))
+
+(defn assign-timestamp-and-watermarks [consumer assigner]
+  (.assignTimestampsAndWatermarks assigner))
+
+(defn create-properties [properties-vector]
+  "Creates a java.util.Properties object. Property strings have to passed as a vector-of-vectors or as a list of list,
+  e.g.: [[key1 value1] [key2 value2] ... ]"
+  (loop [property-object (Properties.)
+         remaining-proberties properties-vector]
+    (if (empty? remaining-proberties)
+      property-object
+      (let [[current & remaining] remaining-proberties
+            [key value] current]
+        (.setProperty property-object key value)
+        (recur property-object remaining)))))
+
