2d5d0f02f555007e5ace0a74b9ad38a6def1fc57
Hendrik Thorun <hendrik.thorun@gmail.com>
2016-12-19 15:34:00 +0100

    project.clj updated

--- project.clj
+++ project.clj
@@ -1,16 +1,13 @@
 (defproject clojure2flink "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A Clojure bridge to Apache Flink."
+  :url "https://github.com/thr0n/clojure2flink"
+  :license {:name "Apache License, Version 2.0"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.apache.flink/flink-java "1.1.3"]
                  [org.apache.flink/flink-clients_2.11 "1.1.3"]
                  [org.apache.flink/flink-streaming-java_2.11 "1.1.3"]
                  [org.apache.flink/flink-connector-wikiedits_2.11 "1.1.3"]
                  [org.apache.flink/flink-connector-kafka-0.9_2.11 "1.1.3"]]
-  ;:aot []
-  ;:java-source-paths ["src/java_parts/interfaces"]
-  ;:main testframes.wikipedia-test
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
