883b3089941135d5f47525ad5379b6690819c22c
Hendrik Thorun <hendrik.thorun@gmail.com>
2017-01-20 13:43:40 +0100

    datastream_test updated

--- test/bridge/datastreams_test.clj
+++ test/bridge/datastreams_test.clj
@@ -5,17 +5,18 @@
   (:import (org.apache.flink.streaming.api.datastream DataStreamSink)))
 
 (def test-environment (stream-execution-environment))
+(def test-text-file "file:\\\\C\\datastream_test.txt")
+(def test-csv-file "file:\\\\C\\datastream_test.csv")
 
-;TODO
-(deftest test-add-sink)
+
+; (deftest test-add-sink) ; TODO find a way to mock kafka
 
 (deftest test-print-stream
   (let [test-data-source (from-collection test-environment (list 1 2 3))]
     (is (= (instance? DataStreamSink (print-stream test-data-source)) true))))
 
-;TODO
-(deftest test-write-as-text)
+(deftest test-write-as-text
+  (let [test-data-source (from-collection test-environment (list 1 2 3))]
+    (is (= (instance? DataStreamSink (write-as-text test-data-source test-text-file)) true))))
 
-;TODO
-(deftest test-wirte-as-csv)
-
+; (deftest test-wirte-as-csv) ; TODO test the tuple stuff... in clojure.
